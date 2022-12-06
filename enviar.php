d045ffa98751b135c5d1d19627e30b1d9eaa6ecd
Leonel Saravia <63772108+rocket20@users.noreply.github.com>
2022-12-06 01:39:36 -0600

    Create enviar.php

---
+++ enviar.php
@@ -0,0 +1,22 @@
+<?php 
+
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
+
+
+
+$destinatario = "leonelalvarez.info@gmail.com";
+$asunto = "Contacto desde nuestra web";
+
+$carta = "De: $nombre \n";
+$carta = "Correo: $correo \n";
+$carta = "Telefono: $telefono \n";
+$carta = "Mensaje: $mensaje";
+
+
+mail($destinatario, $asunto, $carta);
+
+
+?>
