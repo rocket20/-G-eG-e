0faec71aa006b89e49cdf10d9c3b42ffa40af5ee
Leonel Saravia <63772108+rocket20@users.noreply.github.com>
2022-12-06 01:11:48 -0600

    Add files via upload

--- enviar.php
+++ enviar.php
@@ -1,25 +1,22 @@
-<?php
-$name = $_POST['name'];
-$mail = $_POST['mail'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
+<?php  
 
-$header = 'From: ' . $mail . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
+// Llamando a los campos
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
 
-$message = "Este mensaje fue enviado por: " . $name . " \r\n";
-$message .= "Su e-mail es: " . $mail . " \r\n";
-$message .= "Teléfono de contacto: " . $phone . " \r\n";
-$message .= "Mensaje: " . $_POST['message'] . " \r\n";
-$message .= "Enviado el: " . date('d/m/Y', time());
+// Datos para el correo
+$destinatario = "codytron.info@gmail.com";
+$asunto = "Contacto desde nuestra web";
 
-$para = 'leonelalvarez.info@gmail.com';
-$asunto = 'Mensaje de... (Escribe como quieres que se vea el remitente de tu correo)';
+$carta = "De: $nombre \n";
+$carta .= "Correo: $correo \n";
+$carta .= "Telefono: $telefono \n";
+$carta .= "Mensaje: $mensaje";
 
-mail($para, $asunto, utf8_decode($message), $header);
+// Enviando Mensaje
+mail($destinatario, $asunto, $carta);
+header('Location:mensaje-de-envio.html');
 
-header("Location:Contáctanos.html");
 ?>
-
